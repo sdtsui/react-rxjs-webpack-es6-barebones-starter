6fa166ac8fccb7ea64ad5f4bb74994abaf65ed1d
sdtsui <szehungdanieltsui@gmail.com>
2016-04-09 11:54:24 -0700

    [wip] add boilerplate app component and observable, bump version

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,25 @@
 // JavaScripts goes here.
+import React, { Component, PropTypes } from 'react'
+import Header from './components/Header'
+import MainSection from './components/MainSection'
+import Intent from '../intent'
+
+class TodoApp extends Component {
+  render() {
+    const { todos, Intent } = this.props
+
+    return (
+      <div>
+        <Header addTodo={Intent.add} />
+        <MainSection todos={todos} Intent={Intent} />
+      </div>
+    )
+  }
+}
+
+TodoApp.propTypes = {
+  todos: PropTypes.array.isRequired,
+  Intent: PropTypes.object.isRequired
+}
+
+export default TodoApp;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 let Rx = require('rx');
 let React = require('react');
 let ReactDOM = require('react-dom');
+
+let Model = {
+  subject : new Rx.ReplaySubject(1);
+};
+
+let Observable = Model.subject.map((appState) => {return appState;});
+
